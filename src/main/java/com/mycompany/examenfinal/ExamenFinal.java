1da8761f3142f2e600e33840d2a452b222475df5
Jimena <3502659090401@ingenieria.usac.edu.gt>
2026-05-02 12:04:24 -0600

    feat: navegación y eliminación, metodos agregados en clase Playlist

--- src/main/java/com/mycompany/examenfinal/ExamenFinal.java
+++ src/main/java/com/mycompany/examenfinal/ExamenFinal.java
@@ -9,17 +9,29 @@ package com.mycompany.examenfinal;
  * @author pichi
  */
 public class ExamenFinal {
+    
     public static void main(String[] args) {
 
         Playlist lista = new Playlist();
 
         Cancion c1 = new Cancion("La mentira", 180, "Luis Miguel");
-        Cancion c2 = new Cancion("La canción", 210, "Bad Bunny");
+        Cancion c2 = new Cancion("La cancion", 210, "Bad Bunny");
         Cancion c3 = new Cancion("Algo contigo", 200, "Los Panchos");
 
         lista.agregarCancion(c1);
         lista.agregarCancion(c2);
         lista.agregarCancion(c3);
+
+        lista.mostrarPlaylist();
+
+        System.out.println("---- SIGUIENTE ----");
+        lista.siguiente();
+
+        System.out.println("---- ANTERIOR ----");
+        lista.anterior();
+
+        System.out.println("---- ELIMINAR ACTUAL ----");
+        lista.eliminarActual();
 
         lista.mostrarPlaylist();
     }
--- src/main/java/com/mycompany/examenfinal/Playlist.java
+++ src/main/java/com/mycompany/examenfinal/Playlist.java
@@ -5,9 +5,99 @@
 package com.mycompany.examenfinal;
 
 /**
- *
- * @author pichi
+ * @author Jimena
  */
 public class Playlist {
+
+    private NodoDoble cabeza;
+    private NodoDoble cola;
+    private NodoDoble actual;
+    private int tamanio;
+
+    public Playlist() {
+        cabeza = null;
+        cola = null;
+        actual = null;
+        tamanio = 0;
+    }
+
+    public void agregarCancion(Cancion c) {
+        NodoDoble nuevo = new NodoDoble(c);
+
+        if (cabeza == null) {
+            cabeza = nuevo;
+            cola = nuevo;
+            actual = nuevo;
+        } else {
+            cola.siguiente = nuevo;
+            nuevo.anterior = cola;
+            cola = nuevo;
+        }
+
+        tamanio++;
+    }
+
+    public void mostrarPlaylist() {
+        NodoDoble temp = cabeza;
+
+        System.out.println("----- PLAYLIST -----");
+
+        while (temp != null) {
+            System.out.println(temp.dato.getTitulo() + " - " + temp.dato.getArtista());
+            temp = temp.siguiente;
+        }
+
+        System.out.println("--------------------");
+    }
+    
+    public void siguiente() {
+        if (actual != null && actual.siguiente != null) {
+            actual = actual.siguiente;
+            actual.dato.reproducir();
+        } else {
+            System.out.println("Ya estas en la ultima cancion");
+        }
+    }
+
+    public void anterior() {
+        if (actual != null && actual.anterior != null) {
+            actual = actual.anterior;
+            actual.dato.reproducir();
+        } else {
+            System.out.println("Ya estas en la primera cancion");
+        }
+    }
+
+    public void eliminarActual() {
+
+        if (actual == null) return;
+
+        if (cabeza == cola) {
+            cabeza = null;
+            cola = null;
+            actual = null;
+        }
+
+        else if (actual == cabeza) {
+            cabeza = cabeza.siguiente;
+            cabeza.anterior = null;
+            actual = cabeza;
+        }
+
+        else if (actual == cola) {
+            cola = cola.anterior;
+            cola.siguiente = null;
+            actual = cola;
+        }
+       
+        else {
+            actual.anterior.siguiente = actual.siguiente;
+            actual.siguiente.anterior = actual.anterior;
+            actual = actual.siguiente;
+        }
+
+        tamanio--;
+        System.out.println("Cancion eliminada");
+    }
     
 }
